fdc967fc8c1a39dcc8afe18d9d02f63ed1601c9b
jhhj424 <jhhj424@naver.com>
2020-07-14 00:28:09 +0900

    200714 프로그래머스 짝수와홀수

---
+++ src/com/jito/solved/programmers/level1_5.java
@@ -0,0 +1,10 @@
+package com.jito.solved.programmers;
+
+public class level1_5 {
+    public static void main(String[] args) {
+        System.out.println(짝수와홀수1(3));
+    }
+    public static String 짝수와홀수1(int num) {
+        return num%2==0?"Even":"Odd";
+    }
+}
