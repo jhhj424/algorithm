fa9a9cd2121f254511cad1a0ae3e5a3b62293c10
jhhj424 <jhhj424@naver.com>
2020-07-14 00:21:37 +0900

    20071 프로그래머스 가운데글자가져오기

---
+++ src/com/jito/solved/programmers/level1_4.java
@@ -0,0 +1,22 @@
+package com.jito.solved.programmers;
+
+public class level1_4 {
+    public static void main(String[] args) {
+        System.out.println(가운데글자가져오기("abcde"));
+        System.out.println(가운데글자가져오기2("shinjiho"));
+    }
+    public static String 가운데글자가져오기(String s) {
+        String answer = "";
+        int len = s.length()/2;
+        if(s.length()%2==0) { // 짝수
+            answer = s.substring(len-1, len+1);
+        }else {  // 홀수
+            answer = s.substring(len, len+1);
+        }
+        return answer;
+    }
+    public static String 가운데글자가져오기2(String s) {
+        return s.substring((s.length()-1) / 2, s.length()/2 + 1);
+    }
+
+}
