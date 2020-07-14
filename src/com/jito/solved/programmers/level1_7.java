5bd7ed8bf56dbef501b8feb63df61af28b70ba4c
jiho <jhhj424@naver.com>
2020-07-15 01:06:21 +0900

    20200725

--- src/com/jito/solved/programmers/level1_7.java
+++ src/com/jito/solved/programmers/level1_7.java
@@ -2,7 +2,9 @@ package com.jito.solved.programmers;
 
 import java.util.Arrays;
 /*
-* 문제 설명
+20200715
+
+문제 설명
 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
 예를 들어 s가 pPoooyY면 true를 return하고 Pyy라면 false를 return합니다.
