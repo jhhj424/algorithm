cb17ed2582a5f7aa84fbb4ded1d8a01eaf8ea691
jiho <jhhj424@naver.com>
2020-08-15 21:16:49 +0900

    프로그래머스 레벨2 - 스킬트리

--- src/com/jito/solved/programmers/level_2_test/SkilTree.java
+++ src/com/jito/solved/programmers/level_2_test/SkilTree.java
@@ -12,22 +12,19 @@ public class SkilTree {
             boolean flag = true;
 
             // 선행 스킬트리 배열 반복
-            for(int j=1; j<advanceSkill.length; j++) {
-
+            for(int j=1, k=0; j<advanceSkill.length; j++,k++) {
+                int start = skill_trees[i].indexOf(advanceSkill[k]);
+                int end = skill_trees[i].indexOf(advanceSkill[j]);
                 // 선행스킬은 없는데 후행스킬트리가 있는경우
-                if(skill_trees[i].indexOf(advanceSkill[j-1]) == -1 && skill_trees[i].indexOf(advanceSkill[j]) != -1) {
+                if(start == -1 && end != -1) {
                     flag = false;
                 }
-
                 // 후행스킬이 있을때 선행스킬보다 후행스킬이 더 앞에있는경우
-                if(skill_trees[i].indexOf(advanceSkill[j-1]) > skill_trees[i].indexOf(advanceSkill[j]) && skill_trees[i].indexOf(advanceSkill[j]) != -1) {
+                if(end != -1 && start > end) {
                     flag = false;
                 }
             }
-            if(flag) {
-                System.out.println(skill_trees[i]);
-                answer++;
-            }
+            if(flag) answer++;
         }
         return answer;
     }
