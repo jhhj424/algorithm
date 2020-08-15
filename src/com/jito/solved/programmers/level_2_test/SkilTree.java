77a76de6f68c536aa1627115be61fff1c2b45858
jiho <jhhj424@naver.com>
2020-08-15 18:58:16 +0900

    프로그래머스 레벨2 - 스킬트리

--- src/com/jito/solved/programmers/level_2_test/SkilTree.java
+++ src/com/jito/solved/programmers/level_2_test/SkilTree.java
@@ -31,10 +31,27 @@ public class SkilTree {
         }
         return answer;
     }
+    public int solution2(String skill, String[] skill_trees) {
+        int answer = 0;
+        ArrayList<String> skillTrees = new ArrayList<>(Arrays.asList(skill_trees));
+        Iterator<String> it = skillTrees.iterator();
+
+        // skilTrees가 있으면 반복
+        while (it.hasNext()) {
+            // skilTrees 요소에서 replaceAll + 정규표현식을 사용하여 skill에 포함된 문자가 아니면 "" 로변환
+            // 변환한 문자가 skill에 있는지 확인 그리고 순서대로인지 확인하기위해서 0번째 인덱스가 아니면 해당 list 요소는 삭제처리
+            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
+                it.remove();
+            }
+        }
+        answer = skillTrees.size();
+        return answer;
+    }
     public static void main(String[] args) {
         SkilTree st = new SkilTree();
         String s = "CBD";
         String arr[] = {"BACDE", "CBADF", "AECB", "BDA"};
         System.out.println(st.solution(s, arr));
+        System.out.println(st.solution2(s, arr));
     }
 }
