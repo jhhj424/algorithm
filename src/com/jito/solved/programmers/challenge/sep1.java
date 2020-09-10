f8e8fc68d0dc34befeb4a6365d18551f035264b9
jiho <jhhj424@naver.com>
2020-09-11 00:21:35 +0900

    서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수

---
+++ src/com/jito/solved/programmers/challenge/sep1.java
@@ -0,0 +1,58 @@
+package com.jito.solved.programmers.challenge;
+
+import java.util.*;
+
+/*
+정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+numbers의 길이는 2 이상 100 이하입니다.
+numbers의 모든 수는 0 이상 100 이하입니다.
+입출력 예
+numbers	result
+[2,1,3,4,1]	[2,3,4,5,6,7]
+[5,0,2,7]	[2,5,7,9,12]
+입출력 예 설명
+입출력 예 #1
+
+2 = 1 + 1 입니다. (1이 numbers에 두 개 있습니다.)
+3 = 2 + 1 입니다.
+4 = 1 + 3 입니다.
+5 = 1 + 4 = 2 + 3 입니다.
+6 = 2 + 4 입니다.
+7 = 3 + 4 입니다.
+따라서 [2,3,4,5,6,7] 을 return 해야 합니다.
+입출력 예 #2
+
+2 = 0 + 2 입니다.
+5 = 5 + 0 입니다.
+7 = 0 + 7 = 5 + 2 입니다.
+9 = 2 + 7 입니다.
+12 = 5 + 7 입니다.
+따라서 [2,5,7,9,12] 를 return 해야 합니다.
+ */
+public class sep1 {
+    public static void main(String[] args) {
+        int[] a = {2,1,3,4,1};
+        int[] r = solution(a);
+        for(int i: r) {
+            System.out.println(i);
+        }
+    }
+    public static int[] solution(int[] numbers) {
+        int[] answer;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                if(!list.contains(numbers[i] + numbers[j]))
+                    list.add(numbers[i] + numbers[j]);
+            }
+        }
+        Collections.sort(list);
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
