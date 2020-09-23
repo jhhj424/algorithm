5cf277466d77b92f88fdaf27043b00e28e964bc6
SHIN JIHO <jhhj424@naver.com>
2020-09-23 21:40:50 +0900

    연습문제 - 정수 내림차순으로 배치하기

---
+++ src/com/jito/solved/programmers/level_1_test/Problem_18.java
@@ -0,0 +1,21 @@
+package com.jito.solved.programmers.level_1_test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+연습문제 - 정수 내림차순으로 배치하기
+
+https://programmers.co.kr/learn/courses/30/lessons/12933?language=java
+ */
+public class Problem_18 {
+    public static void main(String[] args) {
+        System.out.println(solution(118372));// 873211
+    }
+
+    public static long solution(long n) {
+        String[] arr = String.valueOf(n).split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        return Long.parseLong(Arrays.toString(arr).replaceAll("[^0-9]",""));
+    }
+}
