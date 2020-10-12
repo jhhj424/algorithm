5fbe1d09920ca14e045a2571363c06596b8b9167
jiho <jhhj424@naver.com>
2020-10-13 00:51:22 +0900

    백준 13549번 문제 : 숨바꼭질3 : bfs : https://www.acmicpc.net/problem/13549

---
+++ src/com/jito/solved/baekjoon/boj13549.java
@@ -0,0 +1,44 @@
+package com.jito.solved.baekjoon;
+
+import java.util.*;
+
+/*
+ 백준 13549번 문제 : 숨바꼭질3 : bfs : https://www.acmicpc.net/problem/13549
+ */
+public class boj13549 {
+    public static void main(String[] args) {
+        int[] dist = new int[100001];
+        int[] next = new int[3];
+        Arrays.fill(dist, -1); // 거리를 모두 -1로 초기화
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 수빈이 위치
+        int k = sc.nextInt(); // 동생위치 : 목적지
+
+        dist[n] = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+
+            next[0] = now * 2;
+            next[1] = now - 1;
+            next[2] = now + 1;
+
+            for(int i=0; i<3; i++) {
+                if(next[i] < 0 || next[i] > 100000)
+                    continue;
+                if(dist[next[i]] > -1)
+                    continue;
+                if(i == 0) // 시간 0초 증가
+                    dist[next[i]] = dist[now];
+                else // 시간 1초 증가
+                    dist[next[i]] = dist[now] + 1;
+
+                queue.offer(next[i]);
+            }
+        }
+        System.out.println(dist[k]);
+    }
+}
