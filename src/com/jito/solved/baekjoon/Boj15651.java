f235457e8c38e379f0d300f34094ce57643f7a5e
jiho <jhhj424@naver.com>
2021-03-01 13:48:33 +0900

    백준 백트레킹 N과 M (3) : https://www.acmicpc.net/problem/15651

---
+++ src/com/jito/solved/baekjoon/Boj15651.java
@@ -0,0 +1,46 @@
+package com.jito.solved.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 백트레킹 N과 M (3) : https://www.acmicpc.net/problem/15651
+ */
+public class Boj15651 {
+
+    public static int[] arr;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
